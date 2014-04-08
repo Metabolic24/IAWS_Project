5f5f77ec51f8a93dea307f0c7ed0151c2739ccca
Metabolic24 <metabolic.ewilan@hotmail.fr>
2014-04-08 02:22:10 +0200

    Ajout Projet annexe de test Fonctionnalités Java

---
+++ IAWS_Test/src/AvailableBikesService.java
@@ -0,0 +1,5 @@
+
+public interface AvailableBikesService {
+	
+	public BikeStation filterStationsByName(String name);
+}
---
+++ IAWS_Test/src/BikeStation.java
@@ -0,0 +1,191 @@
+//
+// Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.7 
+// Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
+// Généré le : 2014.04.08 à 01:45:34 AM CEST 
+//
+
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour anonymous complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}long"/>
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="adresse" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="availableBikes" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="status" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element ref="{http://www.example.org/TisseoVelib}coordonnees"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "id",
+    "name",
+    "adresse",
+    "availableBikes",
+    "status",
+    "coordonnees"
+})
+@XmlRootElement(name = "bikeStation")
+public class BikeStation {
+
+    protected long id;
+    @XmlElement(required = true)
+    protected String name;
+    @XmlElement(required = true)
+    protected String adresse;
+    protected int availableBikes;
+    @XmlElement(required = true)
+    protected String status;
+    @XmlElement(required = true)
+    protected Coordonnees coordonnees;
+
+    /**
+     * Obtient la valeur de la propriété id.
+     * 
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * Définit la valeur de la propriété id.
+     * 
+     */
+    public void setId(long value) {
+        this.id = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété name.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Définit la valeur de la propriété name.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété adresse.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getAdresse() {
+        return adresse;
+    }
+
+    /**
+     * Définit la valeur de la propriété adresse.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setAdresse(String value) {
+        this.adresse = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété availableBikes.
+     * 
+     */
+    public int getAvailableBikes() {
+        return availableBikes;
+    }
+
+    /**
+     * Définit la valeur de la propriété availableBikes.
+     * 
+     */
+    public void setAvailableBikes(int value) {
+        this.availableBikes = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété status.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * Définit la valeur de la propriété status.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setStatus(String value) {
+        this.status = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété coordonnees.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Coordonnees }
+     *     
+     */
+    public Coordonnees getCoordonnees() {
+        return coordonnees;
+    }
+
+    /**
+     * Définit la valeur de la propriété coordonnees.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Coordonnees }
+     *     
+     */
+    public void setCoordonnees(Coordonnees value) {
+        this.coordonnees = value;
+    }
+
+}
